101ddd9631385bee26ae06205c22a2850f49f05d
Sonia <1737438545@qq.com>
2022-07-01 18:34:39 +0800

    deploy

---
+++ precache-manifest.768b5a9fdc3cdc8afa2d8850b6adf46d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "654ca23a410cda72314f",
+    "url": "/money-website/css/app.897624ce.css"
+  },
+  {
+    "revision": "154696933d44a79a2da87d3c14308829",
+    "url": "/money-website/img/logo.png"
+  },
+  {
+    "revision": "322fbfb5d7cda892c72dc6cd2a5d2884",
+    "url": "/money-website/index.html"
+  },
+  {
+    "revision": "654ca23a410cda72314f",
+    "url": "/money-website/js/app.eb44c7a0.js"
+  },
+  {
+    "revision": "b790aa22071ce7a4ca86",
+    "url": "/money-website/js/chunk-vendors.c6ffaa62.js"
+  },
+  {
+    "revision": "d1e53a66266cb48270287da7eaf25f92",
+    "url": "/money-website/manifest.json"
+  },
+  {
+    "revision": "5dc5d4ec8cf1b2aa6b89a3cdef2c896c",
+    "url": "/money-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-website/robots.txt"
+  }
+]);
